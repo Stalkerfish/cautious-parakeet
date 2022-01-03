0a824338df8fee2735d38cb4e372d50b8894640a
Stalkerfish <silva.allison2002@gmail.com>
2022-01-03 14:56:05 -0300

    add gitignore

--- controllers/my_controller/my_controller.cpp
+++ controllers/my_controller/my_controller.cpp
@@ -8,6 +8,11 @@
 // <webots/DistanceSensor.hpp>, <webots/Motor.hpp>, etc.
 // and/or to add some other includes
 #include <webots/Robot.hpp>
+#include <webots/Motor.hpp>
+#include <webots/DistanceSensor.hpp>
+
+#define MAX_SPEED 6.28
+#define TIME_STEP 64
 
 // All the webots classes are defined in the "webots" namespace
 using namespace webots;
@@ -24,25 +29,58 @@ int main(int argc, char **argv) {
   Robot *robot = new Robot();
 
   // get the time step of the current world.
-  int timeStep = (int)robot->getBasicTimeStep();
-
   // You should insert a getDevice-like function in order to get the
   // instance of a device of the robot. Something like:
   //  Motor *motor = robot->getMotor("motorname");
   //  DistanceSensor *ds = robot->getDistanceSensor("dsname");
   //  ds->enable(timeStep);
 
+  Motor *left_motor = robot->getMotor("left wheel motor");
+  Motor *right_motor = robot->getMotor("right wheel motor");
+  
+  left_motor->setPosition(INFINITY);
+  right_motor->setPosition(INFINITY);
+  
+  left_motor->setVelocity(0.0);
+  right_motor->setVelocity(0.0);
+  
+  double left_speed = MAX_SPEED;
+  double right_speed = MAX_SPEED;
+  
+  DistanceSensor *left_ir = robot->getDistanceSensor("ir0");
+  DistanceSensor *right_ir = robot->getDistanceSensor("ir1");
+  
+  left_ir->enable(TIME_STEP);
+  right_ir->enable(TIME_STEP);
+  
   // Main loop:
   // - perform simulation steps until Webots is stopping the controller
-  while (robot->step(timeStep) != -1) {
+  while (robot->step(TIME_STEP) != -1) {
     // Read the sensors:
     // Enter here functions to read sensor data, like:
     //  double val = ds->getValue();
+    double left_ir_val = left_ir->getValue();
+    double right_ir_val = right_ir->getValue();
 
     // Process sensor data here.
+    left_speed = MAX_SPEED;
+    right_speed = MAX_SPEED;
+
+    bool line_left = (4 < left_ir_val) && (left_ir_val < 15);
+    bool line_right = (4 < right_ir_val) && (right_ir_val < 15);
+
+    // If line on left, drive left
+    if ((left_ir_val > right_ir_val) && line_left){
+	left_speed = MAX_SPEED * 0.25;
+    }
+    else if ((right_ir_val > left_ir_val) && line_right){
+	right_speed = MAX_SPEED * 0.25;
+    }
 
     // Enter here functions to send actuator commands, like:
     //  motor->setPosition(10.0);
+    left_motor->setVelocity(left_speed);
+    right_motor->setVelocity(right_speed);
   };
 
   // Enter here exit cleanup code.
